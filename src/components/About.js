072e7b9877522d319f8c60fa1b5c3fccd83d6930
Biosis Lanjanirina <biosis.lanjanirina@gmail.com>
2021-06-12 12:37:37 +0300

    React Tsk Tracker final commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+
+const About = () => {
+    return (
+        <div>
+           <p>Version: 1.0.0</p>
+            <Link to={"/"}>Go back</Link>
+        </div>
+    );
+};
+
+export default About;
